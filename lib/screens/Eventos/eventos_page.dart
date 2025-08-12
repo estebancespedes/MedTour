d9ab0e1fa8bd4ecbddcb0e213af038953270e51d
estebancespedes <estebancespedesg@gmail.com>
2025-08-12 11:02:07 -0500

    segundo version de el apartado de eventos

--- lib/screens/Eventos/eventos_page.dart
+++ lib/screens/Eventos/eventos_page.dart
@@ -9,19 +9,36 @@ class EventosPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: Text('Eventos',style: Tema().titleStyle(),),centerTitle: true,forceMaterialTransparency: true,),
       body: ListView(
+        padding: EdgeInsets.fromLTRB(10, 5, 10, 5),
         children: [
-          SizedBox(height: 10,),
-          Container(
-            decoration: BoxDecoration(color: const Color.fromARGB(78, 158, 158, 158), borderRadius: BorderRadius.circular(25)),
-            child: ListTile(
-              leading: Icon(Icons.dinner_dining),
-              title: Text('Evento',style: Tema().titleStyle(),),
-              subtitle: Text('Este es un evento (aqui van las fechas)',style: Tema().textStyle(),),
-            ),
-          ),
-          SizedBox(height: 10,),
+          EventElement() 
         ],
       ),
+    );
+  }
+}
+
+class EventElement extends StatelessWidget {
+  const EventElement({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SizedBox(height: 10,),
+        Container(
+          decoration: BoxDecoration(color: const Color.fromARGB(37, 158, 158, 158), borderRadius: BorderRadius.circular(25)),
+          child: ListTile(
+            leading: Icon(Icons.dinner_dining),
+            title: Text('Evento',style: Tema().titleStyle(),),
+            subtitle: Text('Este es un evento (aquí van las fechas).',style: Tema().textStyle(),),
+            onTap: () {},
+          ),
+        ),
+        SizedBox(height: 10,),
+      ],
     );
   }
 }
