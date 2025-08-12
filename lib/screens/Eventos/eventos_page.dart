cbb10ff1eef4c767a3a4d307d6550ad13853e1bc
estebancespedes <estebancespedesg@gmail.com>
2025-08-12 10:49:32 -0500

    primera version de el apartado de eventos

--- lib/screens/Eventos/eventos_page.dart
+++ lib/screens/Eventos/eventos_page.dart
@@ -8,6 +8,20 @@ class EventosPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Eventos',style: Tema().titleStyle(),),centerTitle: true,forceMaterialTransparency: true,),
+      body: ListView(
+        children: [
+          SizedBox(height: 10,),
+          Container(
+            decoration: BoxDecoration(color: const Color.fromARGB(78, 158, 158, 158), borderRadius: BorderRadius.circular(25)),
+            child: ListTile(
+              leading: Icon(Icons.dinner_dining),
+              title: Text('Evento',style: Tema().titleStyle(),),
+              subtitle: Text('Este es un evento (aqui van las fechas)',style: Tema().textStyle(),),
+            ),
+          ),
+          SizedBox(height: 10,),
+        ],
+      ),
     );
   }
 }
